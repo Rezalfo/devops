1d42172f528e978d7c4a19f90a089b552687e9ef
Rezalfo <105159079+Rezalfo@users.noreply.github.com>
2022-09-05 16:28:21 +0200

    Update App.vue
    added error

--- src/App.vue
+++ src/App.vue
@@ -71,8 +71,8 @@ const searchCity = (inputCity) => {
     .then((response) => {
       // handle success
       console.log(response)
-
-      weatherData.value.city = response.data.name
+      
+      weatherDataCauseError.value.city = response.data.name
       weatherData.value.weatherSummary = response.data.weather[0].main
       weatherData.value.weatherDescription = response.data.weather[0].description
       weatherData.value.currentTemperature = response.data.main.temp
